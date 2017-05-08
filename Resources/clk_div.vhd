1d0f6c02c0a6b0f83940d9cb7b769a099801e86c
jsoulsby <jonathansoulsby333@gmail.com>
2017-05-08 16:01:40 +1200

    Tidied up bdf's and fixed clock divider quartering instead halving.

--- Resources/clk_div.vhd
+++ Resources/clk_div.vhd
@@ -2,27 +2,22 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 --50Mhz -> 25Mhz clock divider (50Mhz is provided by the DE0 board)
 entity clk_div is
-port (clk1 : in std_logic;
-       clk : out std_logic
+port (clk_in : in std_logic;
+       clk_halved : out std_logic
      );
 end clk_div;
 
 architecture Behavioral of clk_div is
 
-signal count : integer :=1;
 signal clktemp : std_logic :='0';
 
 begin
 
-process(clk1) 
+process(clk_in) 
 	begin
-		if(clk1'event and clk1='1') then
-			count <=count+1;
-			if(count = 2) then
+		if(clk_in'event and clk_in='1') then
 				clktemp <= not clktemp; --Generates clock at half the speed of input (i.e. 50 -> 25)
-				count <= 1;
-			end if;
-			clk <= clktemp;
+			clk_halved <= clktemp;
 		end if;
 end process;
 end Behavioral;
