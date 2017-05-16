0cb795493527fb3db996edac524c72500c030e03
jsoulsby <jonathansoulsby333@gmail.com>
2017-05-16 12:30:22 +1200

    Score text displays in top left corner.

--- Resources/char_rom.vhd
+++ Resources/char_rom.vhd
@@ -10,13 +10,9 @@ ENTITY char_rom IS
 	PORT
 	(
 		--address			: 	IN STD_LOGIC_VECTOR (8 DOWNTO 0)
-		character_address	:	IN STD_LOGIC_VECTOR (5 DOWNTO 0);		--ASCII code of character to be displayed
-		font_row, font_col	:	IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+		pixel_x				:	IN STD_LOGIC_VECTOR (9 DOWNTO 0);		
+		pixel_y				:	IN STD_LOGIC_VECTOR (9 DOWNTO 0);
 		clock					: 	IN STD_LOGIC ;
-		pixel_x, pixel_y	:	OUT STD_LOGIC_VECTOR(9	downto 0);
-		rom_mux_output		:	OUT STD_LOGIC;
-		horiz_sync_out		:	OUT STD_LOGIC;
-		vert_sync_out		:	OUT STD_LOGIC;
 		red,green,blue		:	OUT STD_LOGIC
 	);
 END char_rom;
@@ -24,18 +20,13 @@ END char_rom;
 
 ARCHITECTURE SYN OF char_rom IS
 
-	SIGNAL rom_data		: STD_LOGIC_VECTOR (7 DOWNTO 0);
-	SIGNAL rom_address	: STD_LOGIC_VECTOR (8 DOWNTO 0);
-	SIGNAL score_on		: STD_LOGIC;
-	
-
-	
-	COMPONENT vga_sync
- 		PORT(clock_25Mhz, red, green, blue	: IN	STD_LOGIC;
-         	red_out, green_out, blue_out	: OUT 	STD_LOGIC;
-			horiz_sync_out, vert_sync_out	: OUT 	STD_LOGIC;
-			pixel_row, pixel_column			: OUT STD_LOGIC_VECTOR(10 DOWNTO 0));
-	END COMPONENT;
+	SIGNAL rom_data															: STD_LOGIC_VECTOR (7 DOWNTO 0);
+	SIGNAL rom_mux_output													: STD_LOGIC;
+	SIGNAL rom_address														: STD_LOGIC_VECTOR (8 DOWNTO 0);
+	SIGNAL text_on, score_on												: STD_LOGIC;
+	SIGNAL pix_y, pix_x														: UNSIGNED(9 DOWNTO 0);
+	SIGNAL font_col, font_col_score, font_row, font_row_score	: STD_LOGIC_VECTOR(2 DOWNTO 0);
+	SIGNAL char_address, char_address_score							: STD_LOGIC_VECTOR(5 downto 0);
 
 	COMPONENT altsyncram
 	GENERIC (
@@ -63,8 +54,9 @@ ARCHITECTURE SYN OF char_rom IS
 
 BEGIN
 	--rom_mux_output	<= sub_wire0(7 DOWNTO 0);
-		
-				
+	pix_x <= unsigned(pixel_x);
+	pix_y <= unsigned(pixel_y);
+	
 	altsyncram_component : altsyncram
 	GENERIC MAP (
 		address_aclr_a => "NONE",
@@ -88,8 +80,41 @@ BEGIN
 		q_a => rom_data
 	);
 	
+	score_on <= '1' when
+		pix_y(9 downto 3) = 4
+		and
+		pix_x(9 downto 3) > 3
+		and
+		pix_x(9 downto 3) < 9
+		else
+		'0';
+	font_row_score <= std_logic_vector(pix_y(2 downto 0));
+	font_col_score <= std_logic_vector(pix_x(2 downto 0));
+	with pix_x(9 downto 3) select
+	char_address_score <=
+	"010011" when "0000100",   -- S (23)
+	"000011" when "0000101",   -- C (03)
+	"001111"	when "0000110",   -- O (17)
+	"010010" when "0000111",   -- R (22)	
+	"000101" when others;   -- E (05)
 	
-	rom_address <= character_address & font_row;
+	process(score_on, char_address_score, font_col_score, font_row_score)
+	begin
+	red <= '1';
+	green <= '1';
+	blue <= '1';	
+	if score_on = '1' then
+		char_address <= char_address_score;
+		font_row <= font_row_score;
+		font_col <= font_col_score;
+		if rom_mux_output = '1' then
+			red <= '0';
+			green <= '0';
+			blue <= '0';
+		end if;
+	end if;
+	end process;
+	text_on <= score_on;
+	rom_address <= char_address & font_row;
 	rom_mux_output <= rom_data (CONV_INTEGER(NOT font_col(2 DOWNTO 0)));
-
 END SYN;
